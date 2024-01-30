f9484d1dba34eb6898b803877d945e00a7ae0879
ixmaheshmaheshwari <157010602+ixmaheshmaheshwari@users.noreply.github.com>
2024-01-30 15:32:32 +0530

    commit

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import artistsData from "./artist.js";
 function createArtistCard(artist) {
   const card = document.createElement("div");
   card.classList.add("col-lg-4", "col-md-6", "col-sm-12", "mb-4");
-  card.style="width:15rem;"
-  card.style="height:auto;"
+  card.style = "width:15rem;"
+  card.style = "height:auto;"
 
   card.innerHTML = `
     <div class="card">
@@ -20,12 +20,30 @@ function createArtistCard(artist) {
 
   return card;
 }
-
-function displayArtistCards(artistData) {
+function NoArtistCardO(term) {
   const container = document.getElementById("rows");
-  container.innerHTML = ''; 
+  container.innerHTML = '';
+  const card = document.createElement("div");
+  card.classList.add("col-lg-4", "col-md-6", "col-sm-12", "mb-4");
+  card.style = "width:15rem;"
+  card.style = "height:15rem;"
 
-  for (const artist of artistData) {
+  card.innerHTML = `
+    <div class="card" style="height: 15rem;">
+      <div class="card-body">
+        <h5 class="card-title"><strong>No such Record found- ${term}</strong></h5>
+        <p class="card-text">No record are found the searched element-${term}. Please search for other element</p>
+      </div>
+    </div>
+  `;
+
+  container.appendChild(card);
+}
+function displayArtistCards(artistData) {
+const container = document.getElementById("rows");
+  container.innerHTML = '';
+
+  for (const artist of artistData){
     const artistCard = createArtistCard(artist);
     container.appendChild(artistCard);
   }
@@ -35,16 +53,21 @@ function searchArtists() {
   const searchInput = document.getElementById("search");
   const searchTerm = searchInput.value.toLowerCase();
   console.log(searchTerm, typeof searchTerm)
-  
+
 
   const filteredArtists = artistsData.filter(artist => {
     const artistInfo = Object.values(artist).join(" ").toLowerCase();
     console.log(artistInfo, typeof artistInfo)
     return artistInfo.includes(searchTerm);
   });
-
+if(filteredArtists.length===0){
+  NoArtistCardO(searchTerm);
+}else{
   displayArtistCards(filteredArtists);
 }
-document.getElementById("searchBtn").addEventListener("click",searchArtists);
-document.getElementById("search").addEventListener("input",searchArtists);
-displayArtistCards(artistsData);
+}
+document.getElementById("searchBtn").addEventListener("click", searchArtists);
+document.getElementById("search").addEventListener("input", searchArtists);
+document.addEventListener('DOMContentLoaded', () =>{
+  displayArtistCards(artistsData);
+});
